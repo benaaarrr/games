a451a194b1b53a72e47fc93fd5e2b4134d2cd832
jimbobjackjill <bengodwin@live.co.uk>
2015-04-16 08:33:20 +0100

    - initial commit

---
+++ breakout/breakout/Paddle.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace breakout
+{
+    public class Paddle : Sprite
+    {
+        public Paddle(Texture2D texture, Vector2 location, Rectangle screenBounds)
+            : base(texture, location, screenBounds)
+        {
+
+        }
+    }
+}
---
+++ breakout/breakout/Sprite.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace breakout
+{
+    public abstract class Sprite
+    {
+
+
+        public Sprite(Texture2D texture, Vector2 location, Rectangle gameBoundaries)
+        {
+            this.texture = texture;
+            this.location = location;
+            this.gameBoundaries = gameBoundaries;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, location, Color.White);               
+        }
+
+        Rectangle gameBoundaries { get; set; }
+        Vector2 location { get; set; }
+        Texture2D texture { get; set; }
+    }
+}
